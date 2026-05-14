823a679c57f50ed68421393c7b0af3ce4aa449eb
michal imrisek <miso.imrisek@gmail.com>
2026-05-14 14:28:58 +0200

    more cleanup, added logging function and seed

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 from random import randrange
 from random import uniform
+from random import seed
 import os
+
+SEED = 4242
+seed(SEED)
 
 best_movement =  [0.09028615342502605,0,-0.009740252911162787,0,0.9813074069726829,0,-0.47865520222657537,0,0.30557278861697523,0,-0.3102728815657489,0,-0.030020618627862015,0,-0.3982886223111034,0,0.11943979386601072,0,0.2184937846571669] 
 best_rotation =  [0.36969438580912795,54.60791672333049,0.33119821539801886,6.037682262039188,0.8350660735035955,-17.431440526432493,-1.8420354407091328,1.1390813438361806,0.2517998107395694,0.6465559356472826]
@@ -83,14 +87,13 @@ a = 1 #the x coordinate of the -start and endpoint of the sofa's journey
 sofa_len = 3.35  #lenght of the starting block of squares, 2*a*(2**(1/2)) + 1 would be using all available space, 3.35 is just a heuristic from known solution
 sofa_width = 1	 #matches the width of the corridor
 
-res_x = 600 #number of pixels along the sofa's length (resolution)
+res_x = 400 #number of pixels along the sofa's length (resolution)
 res_y = int(res_x * sofa_width / sofa_len)
 square_len = sofa_len / res_x
 square_width = sofa_width / res_y
 square_radius = (square_len**2 + square_width**2)**0.5 / 2 # for checking if the square stayed in bounds
-squares = [[True for i in range(res_y)] for i in range(res_x)]
 walls = [Polynomial([-(2**(1/2))/2 - a, -1]), Polynomial([(2**(1/2))/2 - a, -1]), Polynomial([-(2**(1/2))/2 - a, 1]), Polynomial([(2**(1/2))/2 - a, 1])]
-steps = 200 #number of simulation steps, the sofa will take from -a to a
+steps = 120 #number of simulation steps, the sofa will take from -a to a
 
 
 def squares_position(x_idx, y_idx, center_x, center_y, rotation):
@@ -187,97 +190,91 @@ def save_sofa_image(filename, squares):
 	plt.savefig(filename, dpi=150, bbox_inches='tight')
 	plt.close()
 
-def change_polynomial(poly, degree, epsilons, mutation_probability=0.2, even = False):
+def log_solution(movement, rotation, output_ind, generation):
+	output_file = os.path.join(output_dir, f'gen_{generation+1}.png')
+	save_sofa_image(output_file, squares)
+	with open(f'best_solution_log_{output_ind}.txt', 'a') as log_file:
+		log_file.write(f"Generation {generation+1}: movement {movement} rotation {rotation}\n")
+
+def change_polynomial(poly, degree, epsilons, mutation_probability=0.2):#a function to mutate the polynomials
 	coefficients = poly.coefficients[:]
-	while len(coefficients) < degree*2 -1:
+	while len(coefficients) <= degree:
 		coefficients.append(0)
 
 	changed = []
 	for i in range(degree):
-		j = i
-		if even:
-			i = i*2
 		if uniform(0, 1) < mutation_probability:
-			coefficients[i] += uniform(-1, 1) * epsilons[j]
-			changed.append(j)
+			coefficients[i] += uniform(-1, 1) * epsilons[i]
+			changed.append(i)
 
 	if not changed:
 		i = randrange(0, degree)
-		j = i
-		if even:
-			i = i*2
-		coefficients[i] += uniform(-1, 1) * epsilons[j]
-		changed.append(j)
+		coefficients[i] += uniform(-1, 1) * epsilons[i]
+		changed.append(i)
 
 	return Polynomial(coefficients), changed
 
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+def next_output_index(base_dir):#finds unused name for output files, so can run multiple instances
+	i = 0
+	while os.path.isdir(os.path.join(base_dir, f'output_{i}_images')):
+		i += 1
+	return i
+
 base = Polynomial([-a, 1]) * Polynomial([a, 1])  # hits (-a,0) and (a,0) -> start and endpoint of the sofas jurney
-multiplier = Polynomial([1])
+multiplier = Polynomial([1/a]) #the movement polynomial that evolves, the final movement is base*multiplier, base*multiplier is inicialized to hit the center of the corner
 rotation = Polynomial([0])
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-output_ind = 99 #just for running the script multiple times without overwriting previous outputs(could be done smarter)
-output_dir = os.path.join(script_dir, f'output_{output_ind}') 
+output_ind = next_output_index(script_dir)
+output_dir = os.path.join(script_dir, f'output_{output_ind}_images')
+os.makedirs(output_dir)
+
+cover, squares = fitness(base * multiplier, rotation)
+save_sofa_image(os.path.join(output_dir, f'gen_0.png') , squares)
+print(cover * sofa_len * sofa_width / (res_x * res_y))
+
+rotation_degree = 8
+movement_degree = 16 #the movement polynomial is even, so has a lot of zeros
+epsilon_size = 0.1
+epsilons_rot = [20.0 for _ in range(rotation_degree+1)]#default epsilons for the exploration phase
+epsilons_mov = [1 if i % 2 == 0 else 0 for i in range(movement_degree+1)]
+
+generations = 10000
+stagnation = 0
+for generation in range(generations):
+	got_better = False
+	new_rot, changed_rot = change_polynomial(rotation, rotation_degree, epsilons_rot, mutation_probability=0.35)
+	new_mov, changed_mov = change_polynomial(multiplier, movement_degree, epsilons_mov, mutation_probability=0.35)
+	new_cover, new_squares = fitness(base * new_mov, new_rot)
+	if new_cover > cover:
+		cover = new_cover
+		rotation = new_rot
+		multiplier = new_mov
+		squares = new_squares
+		log_solution(base*multiplier, rotation, output_ind, generation)
+
+		if generation > 50: #first 50 generations are for exploration, the epsilons are not based on the coefficients, but just set to a high value to encourage exploration
+			rot_coeffs = rotation.coefficients[:]
+			while len(rot_coeffs) < rotation_degree:
+				rot_coeffs.append(0)
+			epsilons_rot = [max(0.01,  epsilon_size * abs(rot_coeffs[idx])) for idx in range(rotation_degree)]
+
+			mov_coeffs = multiplier.coefficients[:]
+			while len(mov_coeffs) < movement_degree * 2 - 1:
+				mov_coeffs.append(0)
+			epsilons_mov = [max(0.01, epsilon_size * abs(mov_coeffs[idx])) if idx % 2 == 0 else 0 for idx in range(movement_degree)]
+
+	elif stagnation > 100:		
+		stagnation = 0
+#		square_len = sofa_len / res_x
+#		square_width = sofa_width / res_y
+#		steps = min(int(steps*1.2), 300)
+#		cover, squares = fitness(base * multiplier, rotation)]
+	else:
+		stagnation += 1
 
 
-if __name__ == '__main__':
-	os.makedirs(output_dir, exist_ok=True)
-
-	c, squares = fitness(base * multiplier, rotation)
-	print(c * sofa_len * sofa_width / (res_x * res_y))
-
-	rotation_degree = 12
-	movement_degree = 12
-	epsilon_size = 0.1
-	epsilons_rot = [10.0 for _ in range(rotation_degree)]
-	epsilons_mov = [0.1 for _ in range(movement_degree)]
-
-	generations = 100000
-	stagnation = 0
-	for i in range(generations):
-		got_better = False
-		new_rot, changed_rot = change_polynomial(rotation, rotation_degree, epsilons_rot, mutation_probability=0.35)
-		new_mov, changed_mov = change_polynomial(multiplier, movement_degree, epsilons_mov, mutation_probability=0.35, even=True)
-		new_c, new_squares = fitness(base * new_mov, new_rot)
-		print("was", c, "new", new_c)
-		if new_c > c:
-			c = new_c
-			rotation = new_rot
-			multiplier = new_mov
-			squares = new_squares
-
-			if i > 50: #first 50 generations are for exploration, the epsilons are not based on the coefficients, but just set to a high value to encourage exploration
-				rot_coeffs = rotation.coefficients[:]
-				while len(rot_coeffs) < rotation_degree:
-					rot_coeffs.append(0)
-				epsilons_rot = [max(0.05,  epsilon_size * abs(rot_coeffs[idx])) for idx in range(rotation_degree)]
-
-				mov_coeffs = multiplier.coefficients[:]
-				while len(mov_coeffs) < movement_degree * 2 - 1:
-					mov_coeffs.append(0)
-				epsilons_mov = [max(0.005, epsilon_size * abs(mov_coeffs[idx * 2])) for idx in range(movement_degree)]
-
-			got_better = True
-
-		coverage = c * sofa_len * sofa_width / (res_x * res_y)
-		print(f"Generation {i+1}: {coverage:.4f} coverage")
-
-		if got_better:
-			output_file = os.path.join(output_dir, f'gen_{i+1:05d}.png')
-			save_sofa_image(output_file, squares)
-			print(f"Saved image")
-			with open(f'best_solution_log_{output_ind}.txt', 'a') as log_file:
-				log_file.write(f"Generation {i+1}: movement {multiplier} rotation {rotation}\n")
-		else:
-			stagnation += 1
-			if stagnation > 10000:		
-					stagnation = 0
-					res_x = min(int(res_x*1.2), 1000)
-					res_y = int(res_x * sofa_width // sofa_len)
-					square_len = sofa_len / res_x
-					square_width = sofa_width / res_y
-					steps = min(int(steps*1.2), 300)
-					c, squares = fitness(base * multiplier, rotation)
-					
-
-
+	coverage = cover * sofa_len * sofa_width / (res_x * res_y)
+	print(f"Generation {generation+1}: {coverage} coverage")
+	print("covered squares before:", cover, "new:", new_cover)
